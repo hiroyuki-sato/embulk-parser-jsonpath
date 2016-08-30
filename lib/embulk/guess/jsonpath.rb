412b3b819d45adf7317ad45ff5d17d14fb70f987
Hiroyuki Sato <hiroysato@gmail.com>
2016-08-30 09:18:16 +0900

    Implement guess with Ruby

--- lib/embulk/guess/jsonpath.rb
+++ lib/embulk/guess/jsonpath.rb
@@ -1,61 +1,31 @@
+require 'json'
+require 'jsonpath'
 module Embulk
   module Guess
 
-    # TODO implement guess plugin to make this command work:
-    #      $ embulk guess -g "jsonpath" partial-config.yml
-    #
-    #      Depending on the file format the plugin uses, you can use choose
-    #      one of binary guess (GuessPlugin), text guess (TextGuessPlugin),
-    #      or line guess (LineGuessPlugin).
+    class Jsonpath < TextGuessPlugin
+      Plugin.register_guess("jsonpath", self)
 
-    #class Jsonpath < GuessPlugin
-    #  Plugin.register_guess("jsonpath", self)
-    #
-    #  def guess(config, sample_buffer)
-    #    if sample_buffer[0,2] == GZIP_HEADER
-    #      guessed = {}
-    #      guessed["type"] = "jsonpath"
-    #      guessed["property1"] = "guessed-value"
-    #      return {"parser" => guessed}
-    #    else
-    #      return {}
-    #    end
-    #  end
-    #end
+      def guess_text(config, sample_text)
+        parser_config = config.param("parser",:hash)
+        json_path = parser_config.param("root",:string,default: "$")
+        json = JsonPath.new(json_path).on(sample_text).first
+        if( json.kind_of?(Array) )
+          row = json.first
+          raise RuntimeError,"Can't guess row data must be hash" unless row.kind_of?(Hash)
+          columns = Embulk::Guess::SchemaGuess.from_hash_records(json).map do |c|
+            column = {name: c.name, type: c.type}
+            column[:format] = c.format if c.format
+            column
+          end
+          parser_guessed = {"type" => "jsonpath"}
+          parser_guessed["columns"] = columns
+          return {"parser" => parser_guessed}
+        else
+          raise RuntimeError,"Can't guess specified path(#{json_path})"
+        end
 
-    #class Jsonpath < TextGuessPlugin
-    #  Plugin.register_guess("jsonpath", self)
-    #
-    #  def guess_text(config, sample_text)
-    #    js = JSON.parse(sample_text) rescue nil
-    #    if js && js["mykeyword"] == "keyword"
-    #      guessed = {}
-    #      guessed["type"] = "jsonpath"
-    #      guessed["property1"] = "guessed-value"
-    #      return {"parser" => guessed}
-    #    else
-    #      return {}
-    #    end
-    #  end
-    #end
-
-    #class Jsonpath < LineGuessPlugin
-    #  Plugin.register_guess("jsonpath", self)
-    #
-    #  def guess_lines(config, sample_lines)
-    #    all_line_matched = sample_lines.all? do |line|
-    #      line =~ /mypattern/
-    #    end
-    #    if all_line_matched
-    #      guessed = {}
-    #      guessed["type"] = "jsonpath"
-    #      guessed["property1"] = "guessed-value"
-    #      return {"parser" => guessed}
-    #    else
-    #      return {}
-    #    end
-    #  end
-    #end
-
+      end
+    end
   end
 end
