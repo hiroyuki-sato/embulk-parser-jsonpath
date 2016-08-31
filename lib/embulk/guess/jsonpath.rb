c0cb4a6c0077fa5680581b0e4827016422c39c3a
Hiroyuki Sato <hiroysato@gmail.com>
2016-08-31 09:03:14 +0900

    Check all data in Array. Change message

--- lib/embulk/guess/jsonpath.rb
+++ lib/embulk/guess/jsonpath.rb
@@ -11,8 +11,8 @@ module Embulk
         json_path = parser_config.param("root",:string,default: "$")
         json = JsonPath.new(json_path).on(sample_text).first
         if( json.kind_of?(Array) )
-          row = json.first
-          raise RuntimeError,"Can't guess row data must be hash" unless row.kind_of?(Hash)
+          no_hash = json.find{ |j| !j.kind_of?(Hash) }
+          raise RuntimeError,"Can't exec guess. The row data must be hash." if no_hash
           columns = Embulk::Guess::SchemaGuess.from_hash_records(json).map do |c|
             column = {name: c.name, type: c.type}
             column[:format] = c.format if c.format
@@ -22,7 +22,7 @@ module Embulk
           parser_guessed["columns"] = columns
           return {"parser" => parser_guessed}
         else
-          raise RuntimeError,"Can't guess specified path(#{json_path})"
+          raise RuntimeError,"Can't guess specified the JSONPath: #{json_path}. The results does not return an Array."
         end
 
       end
