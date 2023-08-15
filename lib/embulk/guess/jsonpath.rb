74eedd12a9e75ee2c0120071e816f1111ca1001e
chikamura <mchikamura01@gmail.com>
2023-08-15 10:47:05 +0900

    add comment

--- lib/embulk/guess/jsonpath.rb
+++ lib/embulk/guess/jsonpath.rb
@@ -10,6 +10,10 @@ module Embulk
         parser_config = config.param("parser",:hash)
         json_path = parser_config.param("root",:string,default: "$")
         json = JsonPath.new(json_path).on(sample_text).first
+
+        # The rest of code expect a data type like `[ {}, {}, {}]`.
+        # If JSONPath specifies a Hash object, it needs to pack a hash object into an array.
+        # ex. { "name": "bob", "age":24" } -> [ { "name": "bob", "age":24 } ]
         json = [json] unless json.is_a?(Array)
 
         no_hash = json.find{ |j| !j.kind_of?(Hash) }
